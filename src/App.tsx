7bdb060f757ce164e5255e0aceeb37ff0b4d9142
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-02-11 20:56:38 -0300

    feat: Create top of page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,18 @@ function App() {
 
   return (
     <>
+      <header>
+        <div>
+          <h1>Portfólio</h1>
+        </div>
+
+        <nav>
+          <p>Sobre mim</p>
+          <p>Projetos</p>
+          <p>Serviços</p>
+          <p>Minhas skills</p>
+        </nav>
+      </header>
     </>
   )
 }
