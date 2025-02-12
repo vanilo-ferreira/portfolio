abb4dae0af4e4f1081d79dc1263ea8023b2ecb37
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-02-11 21:52:56 -0300

    feat: Add About Me section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import Image1 from './assets/image1.png'
+import ProfileIcon from './assets/profileIcon.svg'
+import EmailIcon from './assets/emailIcon.svg'
+import InstagramIcon from './assets/instagramIcon.svg'
+import PhoneIcon from './assets/phoneIcon.svg'
 
 function App() {
 
@@ -28,8 +32,66 @@ function App() {
             </div>
 
             <div>
-              <img src={Image1} alt='Waving cartoon'/>
+              <img src={Image1} alt='Waving cartoon' />
             </div>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h2>Sobre mim</h2>
+
+            <p>
+              Recém-formado em Sistemas de Informação com conhecimentos em linguagens de
+              programação, banco de dados e desenvolvimento web. Habilidade em resolução de
+              problemas e aprendizagem rápida.
+              Busco oportunidades para aplicar meu conhecimento no intuito de me desenvolver
+              profissionalmente, enquanto auxilio no crescimento da empresa.
+            </p>
+          </div>
+
+          <div>
+
+            <div>
+              <div>
+                <img src={ProfileIcon} alt='Waving cartoon' />
+              </div>
+              <div>
+                <h4>Meu Nome</h4>
+                <p>Vanilo Ferreira</p>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <img src={EmailIcon} alt='Waving cartoon' />
+              </div>
+              <div>
+                <h4>E-mail</h4>
+                <p>vanilo.ferreira03@gmail.com</p>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <img src={InstagramIcon} alt='Waving cartoon' />
+              </div>
+              <div>
+                <h4>Instagram</h4>
+                <p>vsf_erreira</p>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <img src={PhoneIcon} alt='Waving cartoon' />
+              </div>
+              <div>
+                <h4>Telefone</h4>
+                <p>(71) 9 9309-2086</p>
+              </div>
+            </div>
+
           </div>
         </section>
       </main>
