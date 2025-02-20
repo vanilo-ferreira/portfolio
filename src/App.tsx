81a3b926da015e2a9ad3d5b9cbf8e24783fea7c6
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-02-19 22:39:38 -0300

    fix: Remove Projects section

--- src/App.tsx
+++ src/App.tsx
@@ -29,60 +29,6 @@ function App() {
         <AboutMe />
         <Projects />
 
-        <section id="projects">
-          <h2 className="subtitles">Projetos</h2>
-
-          <div className="projectCards">
-            <div className="card">
-              <div className="cardImage"></div>
-              <div className="cardDetails">
-                <p className="cardTitle">Título do Projeto</p>
-                <p className="stacks">Tecnologias: HTML, CSS e JS.</p>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="cardImage"></div>
-              <div className="cardDetails">
-                <p className="cardTitle">Título do Projeto</p>
-                <p className="stacks">Tecnologias: HTML, CSS e JS.</p>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="cardImage"></div>
-              <div className="cardDetails">
-                <p className="cardTitle">Título do Projeto</p>
-                <p className="stacks">Tecnologias: HTML, CSS e JS.</p>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="cardImage"></div>
-              <div className="cardDetails">
-                <p className="cardTitle">Título do Projeto</p>
-                <p className="stacks">Tecnologias: HTML, CSS e JS.</p>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="cardImage"></div>
-              <div className="cardDetails">
-                <p className="cardTitle">Título do Projeto</p>
-                <p className="stacks">Tecnologias: HTML, CSS e JS.</p>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="cardImage"></div>
-              <div className="cardDetails">
-                <p className="cardTitle">Título do Projeto</p>
-                <p className="stacks">Tecnologias: HTML, CSS e JS.</p>
-              </div>
-            </div>
-          </div>
-        </section>
-
         <section id="services">
           <h2 className="subtitles">Serviços</h2>
 
