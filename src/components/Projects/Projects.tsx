2b9057a287fbf54bd1a06d1b31cc70bfeb73e4d4
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-02-23 00:54:35 -0300

    feat: Make the Projects component dynamic

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,7 +1,9 @@
 import './style.css';
 
 import { useState } from 'react';
-import { Modal } from '../Modal/Modal'
+import { Modal } from '../Modal/Modal';
+
+import { projects } from '../../data/data';
 
 export function Projects() {
     const [open, setOpen] = useState<boolean>(false);
@@ -10,59 +12,32 @@ export function Projects() {
         <div id="projects">
             <h2 className="subtitles">Projetos</h2>
 
-            <Modal
-                isOpen={open}
-                setOpen={setOpen}
-            />
-
             <div className="projectCards">
-                <div className="card" onClick={() => setOpen(!open)}>
-                    <div className="cardImage"></div>
-                    <div className="cardDetails">
-                        <p className="cardTitle">Título do Projeto</p>
-                        <p className="stacks">Tecnologias: HTML, CSS e JS.</p>
-                    </div>
-                </div>
+                {projects.map((project) => (
+                    <>
+                        <Modal
+                            isOpen={open}
+                            setOpen={setOpen}
+                            title={project.title}
+                            subtitle={project.subtitle}
+                            description={project.description}
+                            deploy={project.deploy}
+                            githubLink={project.githubLink}
+                            projectImage={project.projectImage}
+                            technologies={project.technologies}
+                        />
 
-                <div className="card">
-                    <div className="cardImage"></div>
-                    <div className="cardDetails">
-                        <p className="cardTitle">Título do Projeto</p>
-                        <p className="stacks">Tecnologias: HTML, CSS e JS.</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="cardImage"></div>
-                    <div className="cardDetails">
-                        <p className="cardTitle">Título do Projeto</p>
-                        <p className="stacks">Tecnologias: HTML, CSS e JS.</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="cardImage"></div>
-                    <div className="cardDetails">
-                        <p className="cardTitle">Título do Projeto</p>
-                        <p className="stacks">Tecnologias: HTML, CSS e JS.</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="cardImage"></div>
-                    <div className="cardDetails">
-                        <p className="cardTitle">Título do Projeto</p>
-                        <p className="stacks">Tecnologias: HTML, CSS e JS.</p>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <div className="cardImage"></div>
-                    <div className="cardDetails">
-                        <p className="cardTitle">Título do Projeto</p>
-                        <p className="stacks">Tecnologias: HTML, CSS e JS.</p>
-                    </div>
-                </div>
+                        <div className='card' onClick={() => setOpen(!open)}>
+                            <div className='cardImage'>
+                                <img src={project.projectImage} />
+                            </div>
+                            <div className='cardDetails'>
+                                <p className='cardTitle'>{project.title}</p>
+                                <p className='stacks'>Tecnologias: {projects.map(technology => technology.technologies.map(technology => technology.technology).join(", "))}.</p>
+                            </div>
+                        </div>
+                    </>
+                ))};
             </div>
         </div>
     )
