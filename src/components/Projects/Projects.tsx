ae4a735a2ce709c7e52db92ce1734fa46f0194cb
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-02-22 22:02:52 -0300

    feat: Import the Modal component

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,12 +1,22 @@
 import './style.css';
 
+import { useState } from 'react';
+import { Modal } from '../Modal/Modal'
+
 export function Projects() {
+    const [open, setOpen] = useState<boolean>(false);
+
     return (
         <div id="projects">
             <h2 className="subtitles">Projetos</h2>
 
+            <Modal
+                isOpen={open}
+                setOpen={setOpen}
+            />
+
             <div className="projectCards">
-                <div className="card">
+                <div className="card" onClick={() => setOpen(!open)}>
                     <div className="cardImage"></div>
                     <div className="cardDetails">
                         <p className="cardTitle">Título do Projeto</p>
