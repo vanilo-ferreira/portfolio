8aa74f5cf2ecc763e6059fb89f2753df304928ce
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-03-29 22:17:43 -0300

    refactor: Change validator alerts

--- src/validations/formValidation.ts
+++ src/validations/formValidation.ts
@@ -2,10 +2,10 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup"
 
 const formValidationSchema = yup.object({
-    name: yup.string().required('Preencha o nome'),
-    email: yup.string().email('Preencha um e-mail válido').required('Preencha o e-mail'),
-    subject: yup.string().required('Preencha um assunto'),
-    message: yup.string().required('Preencha o conteúdo'),
+    name: yup.string().required('Digite o seu nome'),
+    email: yup.string().email('Preencha um e-mail válido').required('Informe o seu e-mail'),
+    subject: yup.string().required('Informe o assunto'),
+    message: yup.string().required('Digite uma mensagem'),
 }); 
 
 export const formValidationRevolser = yupResolver(formValidationSchema);
