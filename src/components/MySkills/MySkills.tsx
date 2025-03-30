9b0cfc2746f5cf88bf34f802c33ee4db33ca6aaf
Vanilo Ferreira <vanilo.ferreira03@gmail.com>
2025-03-29 22:06:17 -0300

    feat: Add carousel effect

--- src/components/MySkills/MySkills.tsx
+++ src/components/MySkills/MySkills.tsx
@@ -1,4 +1,8 @@
-import './style.css'; 
+import './style.css';
+
+import { useState, useEffect } from "react";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 import HTMLIcon from '../../assets/icons/htmlIcon.svg';
 import CSSIcon from '../../assets/icons/cssIcon.svg';
@@ -7,41 +11,72 @@ import TailwindIcon from '../../assets/icons/tailwindIcon.svg';
 import ReactIcon from '../../assets/icons/reactIcon.svg';
 import NextIcon from '../../assets/icons/nextjsIcon.svg';
 
+
+const skills = [
+    { icon: HTMLIcon, name: "HTML" },
+    { icon: CSSIcon, name: "CSS" },
+    { icon: JSIcon, name: "JAVASCRIPT" },
+    { icon: TailwindIcon, name: "TAILWIND" },
+    { icon: ReactIcon, name: "REACT" },
+    { icon: NextIcon, name: "NEXT.js" },
+];
+
 export function MySkills() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [isPaused, setIsPaused] = useState(false);
+    const visibleSlides = 6;
+    const totalSlides = skills.length;
+
+    useEffect(() => {
+        if (isPaused) return;
+        const interval = setInterval(() => {
+            nextSlide();
+        }, 2000);
+
+        return () => clearInterval(interval);
+    }, [currentIndex, isPaused]);
+
+
+    const nextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % totalSlides);
+    };
+
+    const prevSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + totalSlides) % totalSlides);
+    };
+
+    const getVisibleSlides = () => {
+        return [...skills, ...skills].slice(currentIndex, currentIndex + visibleSlides);
+    };
+
     return (
         <div id='mySkills'>
             <h2 className='subtitles'>Minhas skills</h2>
 
             <div className='containerSkills'>
-                <div className='skillCard'>
-                    <img src={HTMLIcon} alt='HTML Logo' />
-                    <p>HTML</p>
-                </div>
+                <ArrowBackIcon
+                    className='arrows'
+                    onClick={prevSlide}
+                    onMouseEnter={() => setIsPaused(true)}
+                    onMouseLeave={() => setIsPaused(false)}
+                />
 
-                <div className='skillCard'>
-                    <img src={CSSIcon} alt='CSS Logo' />
-                    <p>CSS</p>
-                </div>
+                {getVisibleSlides().map((skill, index) => (
+                    <div key={index} className="skillCard"
+                        onMouseEnter={() => setIsPaused(true)}
+                        onMouseLeave={() => setIsPaused(false)}
+                    >
+                        <img src={skill.icon} alt={`${skill.name} Logo`} />
+                        <p>{skill.name}</p>
+                    </div>
+                ))}
 
-                <div className='skillCard'>
-                    <img src={JSIcon} alt='Javascript Logo' />
-                    <p>JAVASCRIPT</p>
-                </div>
-
-                <div className='skillCard'>
-                    <img src={TailwindIcon} alt='Tailwind Logo' />
-                    <p>TAILWIND</p>
-                </div>
-
-                <div className='skillCard'>
-                    <img src={ReactIcon} alt='React Logo' />
-                    <p>REACT</p>
-                </div>
-
-                <div className='skillCard'>
-                    <img src={NextIcon} alt='Next.js Logo' />
-                    <p>NEXT.js</p>
-                </div>
+                <ArrowForwardIcon
+                    className='arrows'
+                    onClick={nextSlide}
+                    onMouseEnter={() => setIsPaused(true)}
+                    onMouseLeave={() => setIsPaused(false)}
+                />;
             </div>
         </div>
     )
